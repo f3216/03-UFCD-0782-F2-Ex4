b9ebde737db988ea971b0edc7392b43bb9481541
f3216 <122741727+f3216@users.noreply.github.com>
2023-01-29 20:23:56 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,18 +1,38 @@
+#!/bin/bash
+
 echo "A executar os testes ..."
 echo
 
 output=$(./a.out)
-expected_output_1="Ola Mundo!"
-expected_output_2="Estou a aprender C."
 
-output_line_1=$(echo "$program_output" | head -n 1)
-output_line_2=$(echo "$program_output" | head -n 2 | tail -n 1)
-
-if [ "$output_line_1" == "$expected_output_1" ] && [ "$output_line_2" == "$expected_output_2" ] ] ; then
-  echo "Aprovado: A saída é correcta"
+if [ $? -eq 0 ] ; then
+  echo "Aprovado: o programa terminou com retorno zero"
 else
-  echo "Esperada a saída '$expected_output_1' mas o programa devolveu: $output_line_1"
-  echo "Esperada a saída '$expected_output_2' mas o programa devolveu: $output_line_2"
+  echo "Falha: o programa não retornou zero"
+  exit 1
+fi
+
+# Verifica se "Nome : Carlos Fonseca" está em uma linha
+if echo "$output" | grep -q "Nome : Carlos Fonseca" ; then
+  echo "Aprovado: O nome está presente na saída"
+else
+  echo "Falha: O nome não está presente na saída"
+  exit 1
+fi
+
+# Verifica se "Data : 14 de Setembro de 2007" está em uma linha diferente
+if echo "$output" | grep -q "Data : 14 de Setembro de 2007" ; then
+  echo "Aprovado: A data está presente na saída"
+else
+  echo "Falha: A data não está presente na saída"
+  exit 1
+fi
+
+# Verifica se "Morada : Santo Tirso" está em uma linha diferente
+if echo "$output" | grep -q "Morada : Santo Tirso" ; then
+  echo "Aprovado: A morada está presente na saída"
+else
+  echo "Falha: A morada não está presente na saída"
   exit 1
 fi
 
